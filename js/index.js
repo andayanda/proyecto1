730963c7200cb462471de08b628f58040fb1ef18
andayanda <carmelaespinola@gmail.com>
2022-08-02 12:59:50 +0200

    cambios en main.css para normalizar el responsive formato tablet.tambien cambie el index.js añadiendo la funcion de aparecer y desaparecer el menu con el boton de menu.

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const navToggle =document.querySelector('#toogleBtn');
+const navMenu =document.querySelector('.nav-menu');
+navToggle.addEventListener('click',()=>{
+    navMenu.classList.toggle('nav-menu_visible');
+});
