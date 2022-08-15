f26aed14087f25d8cce26a513a4a735609541554
andayanda <carmelaespinola@gmail.com>
2022-08-15 18:25:11 +0200

    link de formspree en formularios. Hay un problema con el modal. Sale siempre, aunque el formulario no este completo

--- js/index.js
+++ js/index.js
@@ -15,12 +15,12 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal-close');
 
 openModal.addEventListener('click',(e)=>{ 
-    e.preventDefault();
+    // e.preventDefault();
    
     modal.classList.add('modal--show');
 });
 closeModal.addEventListener('click',(e)=>{ 
-    e.preventDefault();
+    // e.preventDefault();
    
     modal.classList.remove('modal__show');
 });
